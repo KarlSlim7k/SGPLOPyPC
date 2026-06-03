a23f3c85c01662463c91a22cb7f0868bbc5a1c40
KAROL NAHUM DELGADO BERNAL <a23050014@perote.tecnm.mx>
2026-06-02 23:02:39 -0600

    chore: remover script temporal de migracion 018

--- public/_apply_m018.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-$db = getDbConnection();
-$sql = file_get_contents(__DIR__ . '/../database/migrations/018_soporte_tickets_proveedor.sql');
-$db->exec($sql);
-echo "OK\n";
