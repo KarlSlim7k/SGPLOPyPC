c130c6cefb6e4086b53abbb4bbdf8300a3242000
KAROL NAHUM DELGADO BERNAL <a23050014@perote.tecnm.mx>
2026-06-02 23:11:08 -0600

    chore: remover script temporal de migracion 019

--- public/_apply_m019.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/database.php';
-$db = getDbConnection();
-$sql = file_get_contents(__DIR__ . '/../database/migrations/019_propuesta_estatus_retirada.sql');
-$db->exec($sql);
-echo "OK\n";
